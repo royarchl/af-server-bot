072f01b2673f993b51327cd5edc9b484f6d3d1ad
royarchl <royl.developer.2019@gmail.com>
2025-01-26 03:16:59 -0800

    Functioning Implementation of ISteamMatchmakingRulesResponse

---
+++ cpp-src/src/main.cc
@@ -0,0 +1,121 @@
+#include "steam/isteammatchmaking.h"
+#include "steam/steam_api_common.h"
+#include "steam/steamtypes.h"
+#include <chrono>
+#include <steam/steam_api.h>
+
+#include <cstdio>
+#include <string>
+#include <map>
+#include <arpa/inet.h>  // FOR INET_ADDR
+#include <thread>
+
+/*
+ * LINKS:
+ * https://partner.steamgames.com/doc/api/steam_api#SteamAPI_Init
+ * https://partner.steamgames.com/doc/api/ISteamMatchmakingServers#ServerRules
+ */
+
+class CServerRulesResponse : public ISteamMatchmakingRulesResponse
+{
+public:
+    CServerRulesResponse()
+    : m_QueryHandle(k_uAPICallInvalid)
+    {
+    }
+
+    static uint32_t IPStringToHostOrder(const std::string& ipAddress)
+    {
+        uint32_t networkIP = inet_addr(ipAddress.c_str());
+        return ntohl(networkIP);
+    }
+
+    virtual void RulesResponded(const char* pchRule, const char* pchValue) override
+    {
+        printf("Rule: %s, Value: %s\n", pchRule, pchValue);
+    }
+
+    virtual void RulesFailedToRespond() override
+    {
+        printf("Server failed to respond to rules query\n");
+        ResetQuery();
+    }
+
+    virtual void RulesRefreshComplete() override
+    {
+        printf("Rules refresh complete\n");
+
+        for (const auto& rule : m_ServerRules)
+        {
+            printf("%s: %s\n", rule.first.c_str(), rule.second.c_str());
+        }
+
+        ResetQuery();
+    }
+
+    bool QueryServerRules(std::string szIP, uint16 usPort)
+    {
+        ISteamMatchmakingServers* pMatchmakingServers = SteamMatchmakingServers();
+        if (!pMatchmakingServers)
+        {
+            printf("Failed to get matchmaking servers interface\n");
+            return false;
+        }
+
+        m_ServerRules.clear();
+
+        m_QueryHandle = pMatchmakingServers->ServerRules(IPStringToHostOrder(szIP), usPort, this);
+
+        return m_QueryHandle != k_uAPICallInvalid;
+    }
+
+    void CancelQuery()
+    {
+        if (m_QueryHandle != k_uAPICallInvalid)
+        {
+            ISteamMatchmakingServers* pMatchmakingServers = SteamMatchmakingServers();
+            if (pMatchmakingServers)
+            {
+                pMatchmakingServers->CancelServerQuery(m_QueryHandle);
+            }
+            ResetQuery();
+        }
+    }
+
+private:
+    void ResetQuery() { m_QueryHandle = k_uAPICallInvalid; }
+
+    std::map<std::string, std::string> m_ServerRules;
+
+    HServerQuery m_QueryHandle;
+};
+
+
+int main()
+{
+    if (!SteamAPI_Init())
+    {
+        printf("Steam API failed to initialize\n");
+        return -1;
+    }
+
+    // uint32      serverIP   = CServerRulesResponse.IPStringToHostOrder(sserverIP);
+    std::string m_szServerIP   = "47.152.10.229";
+    uint16      m_usServerPort = 27015;
+
+    CServerRulesResponse rulesResponse;
+
+    if (rulesResponse.QueryServerRules(m_szServerIP, m_usServerPort))
+    {
+        while (true)
+        {
+            SteamAPI_RunCallbacks();
+            // ADD SLEEP OR FRAME TIMING HERE
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+
+    SteamAPI_Shutdown();
+
+    return 0;
+}
