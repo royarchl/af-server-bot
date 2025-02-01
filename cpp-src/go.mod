185b7a2d7bb808aac1bcabd0f63c9fd3ac7f6253
royarchl <royl.developer.2019@gmail.com>
2025-02-01 03:48:20 -0800

    Initial commit for new directory

---
+++ cpp-src/go.mod
@@ -0,0 +1,3 @@
+module github.com/royarchl/af-server-bot
+
+go 1.23.5
---
+++ cpp-src/main.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+#cgo CXXFLAGS: -std=c++11
+#cgo LDFLAGS: -L${SRCDIR}/lib -lA2SQuery -lstdc++
+#cgo CFLAGS: -I${SRCDIR}/lib
+#include "a2s_query_handler_wrapper.h"
+*/
+import "C"
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"unsafe"
+)
+
+func getServerRules(ip string, port int) {
+	payload := C.a2s_query_server_rules(C.CString(ip), C.uint16_t(port))
+
+	pPayload := (*C.Payload)(payload)
+
+	rules := pPayload.m_pMapRules
+	size := pPayload.m_unRulesSize
+
+	mapGoRules := make(map[string]string)
+
+	for i := C.size_t(0); i < size; i++ {
+		rulePtr := (*C.ServerRule)(unsafe.Pointer(uintptr(unsafe.Pointer(rules)) + uintptr(i)*unsafe.Sizeof(*rules)))
+
+		goRule := C.GoString(rulePtr.m_pchRule)
+		goValue := C.GoString(rulePtr.m_pchValue)
+
+		mapGoRules[goRule] = goValue
+	}
+
+	fmt.Println(mapGoRules)
+	fmt.Println(mapGoRules["PlayerCount_i"])
+}
+
+// [31 JAN 2025 @ 3:39 PM GMT-8] Forgive me for the shitshow that is about to ensue
+// [31 JAN 2025 @ 7:53 PM GMT-8] Turns out this worked out pretty nicely
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() { // Creates a goroutine for concurrency
+		defer wg.Done() // Mark the task done when the goroutine finishes
+		for range time.Tick(time.Minute * 1) {
+			getServerRules("47.152.10.229", 27015)
+
+			// In here, update the bot status based on players and (server) response
+			// - this is meant to decide between Online and DnD
+		}
+	}()
+
+	fmt.Println("This method is running at the same time.")
+
+	wg.Wait() // Block the main goroutine for an exit that never comes
+}
